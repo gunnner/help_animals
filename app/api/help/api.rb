2589ff7212cc285fa77f138a5ca817736e005060
gunnner <ctrlgrand@gmail.com>
2021-04-04 15:45:02 +0000

    add access-control-expose-headers - add errors handlers

--- app/api/help/api.rb
+++ app/api/help/api.rb
@@ -2,10 +2,12 @@ require 'grape-swagger'
 
 module Help
   class API < Grape::API
+    extend ActiveSupport::Concern
 
     before do
       header['Access-Control-Allow-Origin'] = '*'
       header['Access-Control-Request-Method'] = '*'
+      header['Access-Control-Expose-Headers'] = '*'
     end
 
     version 'v1', using: :path
@@ -29,6 +31,16 @@ module Help
 
     rescue_from Grape::Exceptions::ValidationErrors do |e|
       rack_response(error_message(400, e.message), e.status)
+    end
+
+    included do
+      rescue_from CanCan::AccessDenied do
+        rack_response(error_message(403, 'access denied'), 403)
+      end
+    end
+
+    rescue_from JWT::DecodeError do
+      rack_response(error_message(401, 'invalid token'), 401)
     end
 
     rescue_from :all do |e|
