c21a7380de70de4c22b0ecba47a96c25b2fc8eac
NoxVail <warden876@gmail.com>
2021-04-04 21:41:03 +0300

    deleted forgotten expose_nil

--- app/api/help/entities/animal_details.rb
+++ app/api/help/entities/animal_details.rb
@@ -5,5 +5,5 @@ class Help::Entities::AnimalDetails < Help::Entities::AnimalDetailsBase
   end
   expose :created_at, as: :dateAdded, documentation: { type: String, desc: 'date of created' }
   expose :updated_at, as: :dateLastEdit, documentation: { type: String, desc: 'date of last editing' }
-  expose :edited_by, as: :editedBy, expose_nil: false, documentation: { type: String, desc: 'who has changed an animal' }
+  expose :edited_by, as: :editedBy, documentation: { type: String, desc: 'who has changed an animal' }
 end
