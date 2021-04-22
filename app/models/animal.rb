416a61c8db38f63f550b50fdc5ab2ef843de4f4a
gunnner <ctrlgrand@gmail.com>
2021-04-22 08:03:46 +0000

    add some validation

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,6 +5,6 @@ class Animal < ApplicationRecord
   accepts_nested_attributes_for :image
 
   validates :age, numericality: { greater_than_or_equal_to: 0.1, less_than_or_equal_to: 20 }
-  validates_presence_of :breed, :sex, :color, :responsible_person
+  validates_presence_of :breed, :sex, :color, :responsible_person, :age
   validates_inclusion_of :complex_vaccination, :rabies_vaccination, :sterilization, :animal_has_family, :show_in_gallery, in: [true, false]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,12 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :roles
 
-  validates :login, uniqueness: true, presence: true
-  validates :email, uniqueness: true, format: { with: VALID_EMAIL }, presence: true
+  validates :login, uniqueness: true, presence: true, length: { maximum: 40 }
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL }, presence: true, length: { maximum: 254 }
   validates :password, length: { minimum: 8 }, presence: true, on: :create
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 2..100 }
+  validates :phone1, length: { maximum: 25 }
+  validates :phone2, length: { maximum: 25 }
 
   def assign_default_role
     add_role(:admin) if roles.blank?
