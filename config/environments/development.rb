11a5d5377c9c4938b2a0cb42176e731a765be97a
gunnner <ctrlgrand@gmail.com>
2021-05-05 11:52:37 +0000

    change production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'myprojectdomain.com',
+    # domain:               'myprojectdomain.com',
     user_name:            ENV["MAIL_USERNAME"],
     password:             ENV["MAIL_PASSWORD"],
     authentication:       'plain',
