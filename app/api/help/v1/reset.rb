90a59679710ae0f47c15b03ac1f668f223077f00
gunnner <ctrlgrand@gmail.com>
2021-05-06 07:35:39 +0000

    add params to the reset endpoint

--- app/api/help/v1/reset.rb
+++ app/api/help/v1/reset.rb
@@ -3,6 +3,12 @@ class Help::V1::Reset < Grape::API
   helpers Help::Helpers::Errors
 
   namespace :reset do
+
+    params do
+      requires :token, type: String, allow_blank: false
+      requires :password, type: String, allow_blank: false
+    end
+
     post do
       user = User.find_by(reset_password_token: params[:token].to_s)
 
